315744812067b69348bd145f70d3fa97a1f35940
vovisim_laptop <wova53209@yandex.ru>
2025-03-19 11:26:18 +0300

    практическая 26

---
+++ ProgrammModules/p26.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammModules
+{
+    internal class p26
+    {
+
+        public int t1(int[,] a)
+        {
+
+            int lenSum = a.Length * 2;
+            HashSet<int> uniqSet = new HashSet<int>();
+
+            foreach (int i in a)
+            {
+
+                uniqSet.Add(i);
+
+            }
+
+            return uniqSet.Count;
+
+
+        }
+
+    }
+}
